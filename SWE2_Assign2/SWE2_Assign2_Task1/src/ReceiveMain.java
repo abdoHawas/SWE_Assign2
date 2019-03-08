6cf3e8e9e63c9ae973e6f833885f78b104903a84
Abdo Hawas <abdoHawas@users.noreply.github.com>
2019-03-09 00:25:19 +0200

    Main Classes

---
+++ SWE2_Assign2/SWE2_Assign2_Task1/src/ReceiveMain.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+
+public class ReceiveMain {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+			Receiver r =new Receiver();
+			r.start();
+
+	}
+
+}
